28afb89753dee5cbe83e434715316bcb3e45af9d
Abhijit Gadgil <gabhijit@iitbombay.org>
2021-07-21 11:07:40 +0530

    Added NR-RRC spec for tracking
    We can now parse the NR-RRC spec as well. Added this spec for tracking.
    Also, added a small script to parse a `docx` spec into an ASN file
    (note: this is a quick script with no error checking!).

---
+++ specs/README.md
@@ -0,0 +1,9 @@
+# ASN.1 Spec We are able to Parse
+
+This directory contains the ASN.1 Specs we are able to parse successfully.
+
+Also a simple script `parse_specs.py` can be used to generate the ASN files
+from the specs file. This is a very basic Proof Of Concept script right now.
+The specification file will have to be downloaded on a local path.
+
+This can be further automated to download the specifications.
---
+++ specs/parse_spec.py
@@ -0,0 +1,40 @@
+import sys
+import re
+
+import docx
+
+start_asn = re.compile(r'^-- ASN1START$')
+end_asn = re.compile(r'^-- ASN1STOP$')
+
+def main(spec_file, asn_file):
+
+    doc = docx.Document(spec_file)
+    start_printing = False
+    out_lines = []
+    for para in doc.paragraphs:
+        if re.match(start_asn, para.text):
+            start_printing = True
+        if start_printing:
+            out_lines.append(para.text + "\n")
+        if re.match(end_asn, para.text):
+            start_printing = False
+
+    preclude = [
+            "--\n",
+            f"-- Generated using : {' '.join(sys.argv)}\n",
+            "-- DO NOT EDIT BY HAND\n",
+            "--\n"]
+    with open(asn_file, 'w') as outfile:
+        outfile.writelines(preclude)
+        outfile.writelines(out_lines)
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 4:
+        print("usage: parse_spec.py <spec.docx> -o <out.asn>")
+        sys.exit(1)
+
+    spec_file = sys.argv[1]
+    asn_file = sys.argv[3]
+    main(spec_file, asn_file)
