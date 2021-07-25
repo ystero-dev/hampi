6472e91dff77f2efc4a23ecaaa4faa5c52543d5a
Abhijit Gadgil <gabhijit@iitbombay.org>
2021-07-25 20:44:00 +0530

    housekeeping: 'parser' Internal module.
    Moved the code in `parser::mod` to it's own internal module
    `parser::int`. This makes all top level modules simply `mod` and `use`
    definitions atleast for the Parser and Resolver.

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,9 +1,5 @@
 //! Main Parser module
 //!
-use crate::error::Error;
-use crate::structs::parser::module::Asn1Module;
-use crate::tokenizer::Token;
-
 mod module;
 
 mod utils;
@@ -16,26 +12,5 @@ mod values;
 
 mod oid;
 
-/// Parse the tokens into internal Asn1Module representation
-///
-/// Token obtained from running [`tokenize`][`crate::tokenizer::tokenize] on an ANS file are parsed
-/// into an internal representation of [`Asn1Module`][`crate::structs::Asn1Module`]. Semantic
-/// errors during parsing the tokens are returned as appropriate variant of `Error`.
-pub fn parse<'parser>(tokens: &'parser mut Vec<Token>) -> Result<Vec<Asn1Module>, Error> {
-    // Get rid of the comments, it complicates things
-    tokens.retain(|x| !x.is_comment());
-
-    let mut modules = vec![];
-    let mut total = 0;
-    loop {
-        let (module, consumed) = module::parse_module(&tokens[total..])?;
-        modules.push(module);
-        total += consumed;
-        if total == tokens.len() {
-            break;
-        }
-    }
-    Ok(modules)
-}
-
-// TODO: Test cases, at-least single-module, multiple modules etc.
+mod int;
+pub use int::parse;
