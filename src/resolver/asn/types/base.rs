382e57e9afc511a57f163f978cec4489ecf25ee7
Abhijit Gadgil <gabhijit@iitbombay.org>
2021-08-06 10:40:50 +0530

    Renamed `get_single_value` to `get_single_string_value`
    As we start 'resolving' constraint, it is important to assign types to
    the values. The `TableConstraint` ObjectSet was a `String` so we are
    explicitly calling it so. There will be some iterations before we get it
    right.

--- src/resolver/asn/types/base.rs
+++ src/resolver/asn/types/base.rs
@@ -1,25 +1,58 @@
 use crate::error::Error;
 
-use crate::parser::asn::structs::types::{Asn1BuiltinType, Asn1Type, Asn1TypeKind};
+use crate::parser::asn::structs::types::{
+    Asn1BuiltinType, Asn1Type, Asn1TypeKind, Asn1TypeReference,
+};
 
 use crate::resolver::{
-    asn::structs::types::base::{
-        Asn1ResolvedBitString, Asn1ResolvedBoolean, Asn1ResolvedCharacterString,
-        Asn1ResolvedEnumerated, Asn1ResolvedInteger, Asn1ResolvedObjectIdentifier,
-        Asn1ResolvedOctetString, ResolvedBaseType,
+    asn::structs::types::{
+        base::{
+            Asn1ResolvedBitString, Asn1ResolvedBoolean, Asn1ResolvedCharacterString,
+            Asn1ResolvedEnumerated, Asn1ResolvedInteger, Asn1ResolvedObjectIdentifier,
+            Asn1ResolvedOctetString, ResolvedBaseType,
+        },
+        constraints::Asn1ConstraintValueSet,
     },
     Resolver,
 };
 
+impl Asn1Type {
+    // Returns the Integer ValueSet for a given Type.
+    //
+    // If the type is a `Base` type, it should be INTEGER or it's an Error. If the `Type` is a
+    // Referenced Type, it should be possible to 'resolve' the Reference to a proper
+    // `Asn1ConstraintValueSet` else it's an error, we'll try to 'recursively' 'resolve' till we
+    // get to a `Base` Type.
+    pub(crate) fn get_integer_valueset_from_constraint(
+        &self,
+        resolver: &Resolver,
+    ) -> Result<Asn1ConstraintValueSet<i64>, Error> {
+        let kind = &self.kind;
+        match kind {
+            Asn1TypeKind::Builtin(Asn1BuiltinType::Integer(ref _i)) => {
+                Err(constraint_error!("Not Implemented!"))
+            }
+            Asn1TypeKind::Reference(Asn1TypeReference::Reference(ref _r)) => {
+                Err(constraint_error!("Not Implemented!"))
+            }
+            _ => Err(constraint_error!(
+                "The Type '{:#?}' is not of a BuiltIn Or a Referenced Kind!",
+                self,
+            )),
+        }
+    }
+}
+
 pub(crate) fn resolve_base_type(
     ty: &Asn1Type,
-    _resolver: &Resolver,
+    resolver: &Resolver,
 ) -> Result<ResolvedBaseType, Error> {
     if let Asn1TypeKind::Builtin(ref kind) = ty.kind {
         match kind {
             Asn1BuiltinType::Integer(ref _i) => {
-                let resolved = ResolvedBaseType::Integer(Asn1ResolvedInteger::default());
-                Ok(resolved)
+                let mut resolved = Asn1ResolvedInteger::default();
+                resolve_integer(&mut resolved, ty, resolver)?;
+                Ok(ResolvedBaseType::Integer(resolved))
             }
             Asn1BuiltinType::Enumerated(ref _i) => {
                 let resolved = ResolvedBaseType::Enum(Asn1ResolvedEnumerated::default());
@@ -55,4 +88,21 @@ pub(crate) fn resolve_base_type(
     } else {
         Err(resolve_error!("Expected Base Type. Found '{:#?}'", ty))
     }
+}
+
+fn resolve_integer(
+    _base: &mut Asn1ResolvedInteger,
+    ty: &Asn1Type,
+    _resolver: &Resolver,
+) -> Result<(), Error> {
+    // No Constraints
+    if ty.constraints.is_none() {
+        return Ok(());
+    }
+
+    //let (root_values, extensible, additional_values) = ty.resolve_constraint(resolver);
+    // FIXME: Only single constraint for now.
+    //let _constraint = &ty.constraints.as_ref().unwrap()[0];
+
+    Ok(())
 }
