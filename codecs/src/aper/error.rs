5f69c3ac1b16af5342f5da9663f0aff9eeb1e921
Nic Larkin <nplrkn@gmail.com>
2022-04-22 08:58:43 +0200

    Error context

--- codecs/src/aper/error.rs
+++ codecs/src/aper/error.rs
@@ -5,18 +5,28 @@ use std::fmt::Display;
 #[derive(Debug)]
 pub struct Error {
     msg: String,
+    context: Vec<String>,
 }
 
 impl Error {
     pub fn new<T: AsRef<str> + Display>(msg: T) -> Self {
         Error {
             msg: msg.to_string(),
+            context: Vec::new(),
         }
+    }
+    pub fn push_context(mut self, context_elem: &str) -> Self {
+        self.context.push(context_elem.to_string());
+        self
     }
 }
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.msg)
+        if self.context.is_empty() {
+            write!(f, "{}", self.msg)
+        } else {
+            write!(f, "[{}]:{}", self.context.join("."), self.msg)
+        }
     }
 }
 
