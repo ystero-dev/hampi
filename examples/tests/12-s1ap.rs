1985567cac3cd3c01db6e25d09ada7cdee109a5a
Abhijit Gadgil <gabhijit@iitbombay.org>
2023-01-14 11:10:52 +0530

    Added example for S1AP and error example for NGAP.

--- examples/tests/12-s1ap.rs
+++ examples/tests/12-s1ap.rs
@@ -5,5 +5,12 @@ mod s1ap {
 }
 
 fn main() {
-    eprintln!("S1AP");
+    use asn1_codecs::{aper::AperCodec, PerCodecData};
+
+    let decode_str = "0004001A00000300000005C0098134A200080004800411EA000240020000";
+    let decode_hex = hex::decode(decode_str).unwrap();
+    let mut codec_data = PerCodecData::from_slice_aper(&decode_hex);
+    let s1ap_pdu = s1ap::S1AP_PDU::aper_decode(&mut codec_data);
+
+    eprintln!("s1ap_pdu: {:#?}", s1ap_pdu.unwrap());
 }
