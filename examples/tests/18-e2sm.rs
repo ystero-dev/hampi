2de0da9dc8ef5d99fbdf29b50207419663c233a1
Abhijit Gadgil <gabhijit@iitbombay.org>
2023-10-31 16:24:04 +0530

    Added example for E2SM codec test
    Initially we were simply parsing E2SM specificiation, Now also added
    codec example with codec round-trip.

--- examples/tests/18-e2sm.rs
+++ examples/tests/18-e2sm.rs
@@ -5,5 +5,29 @@ mod e2sm {
 }
 
 fn main() {
+    use asn1_codecs::{aper::AperCodec, PerCodecData};
     eprintln!("E2SM");
+
+    let _ = env_logger::init();
+
+    // This will break: Fixed
+    let decode_str = "080000000100000000002043514901200000";
+
+    let decode_hex = hex::decode(decode_str).unwrap();
+    let mut codec_data = PerCodecData::from_slice_aper(&decode_hex);
+    let e2sm_kpmv3_pdu = e2sm::E2SM_KPM_IndicationMessage::aper_decode(&mut codec_data);
+    let e2sm_kpmv3_pdu = e2sm_kpmv3_pdu.unwrap();
+    eprintln!("e2sm_kpmv3_pdu: {:#?}", e2sm_kpmv3_pdu);
+
+    let mut encode_codec_data = PerCodecData::new_aper();
+    let result = e2sm_kpmv3_pdu.aper_encode(&mut encode_codec_data);
+    eprintln!("result: {:#?}", result);
+
+    let e2sm_kpmv3_encoded_data = encode_codec_data.get_inner().unwrap();
+    eprintln!("Encoded: {}", hex::encode(&e2sm_kpmv3_encoded_data));
+    eprintln!("Original: {}", decode_str);
+
+    let e2sm_kpmv3_pdu = e2sm::E2SM_KPM_IndicationMessage::aper_decode(&mut encode_codec_data);
+    let e2sm_kpmv3_pdu = e2sm_kpmv3_pdu.unwrap();
+    eprintln!("e2sm_kpmv3_pdu: {:#?}", e2sm_kpmv3_pdu);
 }
