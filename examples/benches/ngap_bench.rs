6da1b26e39baf9fbc5aa23ec712f033e161373af
Abhijit Gadgil <gabhijit@iitbombay.org>
2023-11-03 09:37:44 +0530

    README updates and new benchmarks
    Updated the README.md file to document currently tested protocol
    specifications.
    
    Added an `encode`/`decode` round trip benchmark.

--- examples/benches/ngap_bench.rs
+++ examples/benches/ngap_bench.rs
@@ -21,5 +21,21 @@ pub fn ngap_decode_bench(c: &mut Criterion) {
     });
 }
 
-criterion_group!(ngap_decode, ngap_decode_bench);
-criterion_main!(ngap_decode);
+pub fn ngap_codec_roundtrip_bench(c: &mut Criterion) {
+    let ngap_data = hex::decode(
+        "0015404a000004001b00084002f898000000000052400f06004d79206c6974746c6520674e420066001f01000000000002f8980001000800800000010002f8390001001881c00013880015400140",
+    ).unwrap();
+
+    c.bench_function("NGAP_decode", |b| {
+        b.iter(|| {
+            let mut codec_data = PerCodecData::from_slice_aper(&ngap_data);
+            let ngap_pdu = ngap::NGAP_PDU::aper_decode(&mut codec_data).unwrap();
+
+            let mut codec_encode_data = PerCodecData::new_aper();
+            let _ = ngap_pdu.aper_encode(&mut codec_encode_data).unwrap();
+        });
+    });
+}
+
+criterion_group!(ngap_codec, ngap_decode_bench, ngap_codec_roundtrip_bench);
+criterion_main!(ngap_codec);
