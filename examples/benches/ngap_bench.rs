133df1ac5900d9937068aec898dcfc86bc7f7012
Abhijit Gadgil <gabhijit@iitbombay.org>
2025-08-23 19:18:32 +0530

    Separated encode benchmark for NGAP
    The encode benchmark was embedded inside the `roundtrip` function,
    separated it out into it's own.
    
    Signed-off-by: Abhijit Gadgil <gabhijit@iitbombay.org>

--- examples/benches/ngap_bench.rs
+++ examples/benches/ngap_bench.rs
@@ -26,7 +26,7 @@ pub fn ngap_codec_roundtrip_bench(c: &mut Criterion) {
         "0015404a000004001b00084002f898000000000052400f06004d79206c6974746c6520674e420066001f01000000000002f8980001000800800000010002f8390001001881c00013880015400140",
     ).unwrap();
 
-    c.bench_function("NGAP_decode", |b| {
+    c.bench_function("NGAP_codec_roundtrip", |b| {
         b.iter(|| {
             let mut codec_data = PerCodecData::from_slice_aper(&ngap_data);
             let ngap_pdu = ngap::NGAP_PDU::aper_decode(&mut codec_data).unwrap();
@@ -37,5 +37,26 @@ pub fn ngap_codec_roundtrip_bench(c: &mut Criterion) {
     });
 }
 
-criterion_group!(ngap_codec, ngap_decode_bench, ngap_codec_roundtrip_bench);
+pub fn ngap_encode_bench(c: &mut Criterion) {
+    let ngap_data = hex::decode(
+        "0015404a000004001b00084002f898000000000052400f06004d79206c6974746c6520674e420066001f01000000000002f8980001000800800000010002f8390001001881c00013880015400140",
+    ).unwrap();
+
+    let mut codec_data = PerCodecData::from_slice_aper(&ngap_data);
+    let ngap_pdu = ngap::NGAP_PDU::aper_decode(&mut codec_data).unwrap();
+
+    c.bench_function("NGAP_encode", |b| {
+        b.iter(|| {
+            let mut codec_encode_data = PerCodecData::new_aper();
+            let _ = ngap_pdu.aper_encode(&mut codec_encode_data).unwrap();
+        });
+    });
+}
+
+criterion_group!(
+    ngap_codec,
+    ngap_encode_bench,
+    ngap_decode_bench,
+    ngap_codec_roundtrip_bench
+);
 criterion_main!(ngap_codec);
